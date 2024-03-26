1f364ef16cc5b6d0307ce41da02196fbfcc70d4e
Denis Morev <morev.denis@gmail.com>
2024-03-26 14:49:54 +0300

    поправить Subsciption

--- src/components/subsciption/subscription.tsx
+++ src/components/subsciption/subscription.tsx
@@ -2,11 +2,9 @@ import "./subscription.css";
 
 const Subscription = () => {
   return (
-    <div className="wrap">
-      <div className="subscription">
-        <div className="subscription_text">Мои подписки</div>
-        <button className="subscription_button">Выбрать подписку</button>
-      </div>
+    <div className="subscription">
+      <div className="subscription_text">Мои подписки</div>
+      <button className="subscription_button">Выбрать подписку</button>
     </div>
   );
 };
